e15fba56f77249e3e6c946c5c0ed83391a90e3c7
Paolo Fagioli <fagioli.paolo@educ.di.unito.it>
2020-08-03 12:50:56 +0200

    update home/navbar

--- php/home.php
+++ php/home.php
@@ -15,37 +15,15 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
 </head>
 <body>
-    <h1>BENVENUTO <?= $name ?></h1>
+    <h1>Hello,  <?= $name ?> !</h1>
 
-    <a href="logout.php">logout</a>
-
-<?php
-
-    function retreive_name($email){
-        $db = database_connection();
-        $rows = $db->query("SELECT name FROM users WHERE email = '$email'");
-
-        try{
-            if($rows){
-                foreach ($rows as $row){
-                        return $row['name'];
-                    } throw new Exception("user not found");
-                }
-            else throw new Exception("query error");
-        } catch(Exception $e){
-            ######### TODO: DA DEFINIRE COSA FARE IN CASO DI ECCEZIONI
-        } finally {
-            $db->close();
-        }
-    }
-
-?>
+    <form>
+        <input id="search-box" type="text" placeholder="Search a book...">
+    </form>
 
 </body>
 </html>
--- php/navbar.php
+++ php/navbar.php
@@ -1,9 +1,20 @@
+<?php
+require_once "top.php";
+require_once "db.inc.php";
+
+if(!isset($_SESSION['email'])){
+    header("Location: index.php");
+    die;
+}
+
+$name = retreive_name($_SESSION['email']);
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Navbar</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="../javascript/navbar.js"></script>
@@ -18,8 +29,8 @@
         <ul class="nav-options">
             <li><a href="#">Home</a> </li>
             <li><a href="#">Categories</a> </li>
-            <li><a href="#">Profile</a> </li>
-            <li><a href="#">Settings</a> </li>
+            <li><a href="#"><?= $name?></a> </li>
+            <li><a href="logout.php">Logout</a> </li>
         </ul>
 
         <div id="burger-menu" class="burger-menu">
@@ -30,5 +41,31 @@
 
     </nav>
 
+    <?php
+
+    function retreive_name($email){
+        $db = database_connection();
+        $rows = $db->query("SELECT name FROM users WHERE email = '$email'");
+
+        try{
+            if($rows){
+                foreach ($rows as $row){
+                    return $row['name'];
+                } throw new Exception("user not found");
+            }
+            else throw new Exception("query error");
+        } catch(Exception $e){
+            ######### TODO: DA DEFINIRE COSA FARE IN CASO DI ECCEZIONI
+        } finally {
+            $db->close();
+        }
+    }
+
+    function search_book($book){
+        $db = database_connection();
+        # TODO
+    }
+
+    ?>
 </body>
 </html>
